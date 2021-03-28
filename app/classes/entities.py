7ada8180e9a5b745b3b52a651043ebaf198b7f75
Simon Tallmyr <simon@tallmyr.se>
2021-03-28 03:05:15 +0100

    They will chase down food

---
+++ app/classes/entities.py
@@ -0,0 +1,59 @@
+import random
+import math
+
+
+class NPC:
+    def __init__(self):
+        self.x = random.randint(0, 50)
+        self.y = random.randint(0, 750)
+        self.w = 5
+        self.h = 5
+        self.speed = 1
+        self.loc = [self.x, self.y]
+        self.draw = (self.x, self.y, self.w, self.h)
+
+    def find_target(self, foods):
+        self.distance = 5000
+        for food in foods:
+            distance = math.dist(self.loc, food.loc)
+            if distance < self.distance:
+                self.target = food
+                self.target_x = food.x
+                self.target_y = food.y
+                self.distance = distance
+
+    def move(self):
+        if self.x < self.target_x:
+            self.x += self.speed
+        elif self.x > self.target_x:
+            self.x -= self.speed
+
+        if self.y < self.target_y:
+            self.y += self.speed
+        elif self.y > self.target_y:
+            self.y -= self.speed
+
+        self.update_loc()
+        self.update_draw()
+
+    def update_loc(self):
+        self.loc = [self.x, self.y]
+
+    def update_draw(self):
+        self.draw = (self.x, self.y, self.w, self.h)
+
+    def eat(self, foods):
+        food_list = []
+        for food in foods:
+            if food.loc != self.loc:
+                food_list.append(food)
+        return food_list
+
+
+class Food:
+    def __init__(self):
+        self.x = random.randint(50, 750)
+        self.y = random.randint(50, 750)
+        self.r = 2.5
+        self.loc = [self.x, self.y]
+        self.draw = (self.x, self.y)
---
+++ main.py
@@ -0,0 +1,72 @@
+# Simple pygame program
+
+
+# Import and initialize the pygame library
+import pygame
+from pygame import display, draw, time
+
+from app.classes.entities import NPC, Food
+from app.config import colour
+
+pygame.init()
+# Setup the clock for a decent framerate
+
+clock = time.Clock()
+
+
+# Set up the drawing window
+
+screen = display.set_mode([800, 800])
+
+# Create npcs
+npcs = [NPC() for _ in range(10)]
+foods = [Food() for _ in range(10)]
+# Create Food
+
+
+# Run until the user asks to quit
+
+running = True
+
+while running:
+
+    # Did the user click the window close button?
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+
+            running = False
+
+    # Setup the game board
+
+    # Set background
+    screen.fill((200, 200, 200))
+
+    # Create the grass
+    draw.rect(screen, (colour.GREEN), (50, 50, 700, 700))
+
+    # Update Food
+    for food in foods:
+        draw.circle(screen, (colour.BLUE), (food.draw), food.r)
+
+    # Update NPCs
+    for npc in npcs:
+        npc.find_target(foods)
+        npc.move()
+        draw.rect(screen, (colour.RED), (npc.draw))
+        foods = npc.eat(foods)
+
+    # Flip the display
+
+    display.flip()
+
+    # Ensure program maintains a rate of 30 frames per second
+
+    clock.tick(144)
+
+
+
+# Done! Time to quit.
+
+pygame.quit()
