e4e2654fca6d4d9b9a83d4f2c094befa9b4fa122
Simon Tallmyr <simon@tallmyr.se>
2021-03-28 18:26:55 +0100

    Spawn and Energy

--- app/classes/entities.py
+++ app/classes/entities.py
@@ -1,8 +1,8 @@
-import random
 import math
+import random
 
 from app.config import config
-
+from app.functions import common
 
 # Use a specific seed
 if config.RANDOM_SEED:
@@ -10,13 +10,13 @@ if config.RANDOM_SEED:
 
 
 class NPC:
-    def __init__(self, colour=None, x=None, y=None, w=None, h=None, speed=None):
+    def __init__(self, colour=None, x=None, y=None, w=None, h=None, energy_usage=None):
         self.colour = colour
         self.x = x
         self.y = y
         self.w = w
         self.h = h
-        self.speed = speed
+        self.energy_usage = energy_usage
         if self.colour is None:
             self.colour = (
                 random.randint(20, 235),
@@ -24,15 +24,18 @@ class NPC:
                 random.randint(20, 235),
             )
         if self.x is None:
-            self.x = random.randint(0, 50)
+            self.x = common.horizontal_spawn()
         if self.y is None:
-            self.y = random.randint(0, 750)
+            self.y = common.vertical_spawn()
         if self.w is None:
-            self.w = random.randint(3, 7)
+            self.w = random.randint(5, 15)
         if self.h is None:
-            self.h = random.randint(3, 7)
-        if self.speed is None:
-            self.speed = random.random()
+            self.h = random.randint(5, 15)
+        if self.energy_usage is None:
+            self.energy_usage = random.random()
+
+        self.pregnant = None
+        self.speed = 1 * (config.ENERGY_TO_SPEED * self.energy_usage)
 
         self.loc = [self.x, self.y]
         self.draw = (self.x, self.y, self.w, self.h)
@@ -49,22 +52,26 @@ class NPC:
                 self.distance = distance
 
     def move(self):
-        if self.x < self.target_x:
-            self.x += self.speed
-        elif self.x > self.target_x:
-            self.x -= self.speed
+        if self.pregnant:
+            self.pregnant -= 1
+        else:
+            if self.x < self.target_x:
+                self.x += self.speed
+            elif self.x > self.target_x:
+                self.x -= self.speed
 
-        if self.y < self.target_y:
-            self.y += self.speed
-        elif self.y > self.target_y:
-            self.y -= self.speed
-        self.energy = self.energy - config.BASE_ENERGY_USE
+            if self.y < self.target_y:
+                self.y += self.speed
+            elif self.y > self.target_y:
+                self.y -= self.speed
+
+        self.energy = self.energy - self.energy_usage * config.BASE_ENERGY_USE
 
         self.update_loc()
         self.update_draw()
 
     def update_loc(self):
-        self.loc = [round(self.x), round(self.y)]
+        self.loc = self.x, self.y
 
     def update_draw(self):
         self.draw = (self.x, self.y, self.w, self.h)
@@ -72,11 +79,22 @@ class NPC:
     def eat(self, foods):
         food_list = []
         for food in foods:
-            if food.loc == self.loc:
+            if math.isclose(food.x, self.x, abs_tol=1) and math.isclose(
+                food.y, self.y, abs_tol=1
+            ):
                 self.energy += config.FOOD_VALUE
             else:
                 food_list.append(food)
         return food_list
+
+    def breed(self, npcs):
+        self.pregnant = None
+        self.energy -= config.FOOD_VALUE
+        npcs.append(
+            NPC(
+                self.colour, self.x + 10, self.y + 10, self.w, self.h, self.energy_usage
+            )
+        )
 
 
 class Food:
